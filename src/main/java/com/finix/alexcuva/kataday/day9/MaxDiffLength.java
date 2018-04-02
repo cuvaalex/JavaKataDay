4af8e889686622afa921b86b3ac8b09b69ef0bd5
Alexandre Cuva <alexandre.cuva@gmail.com>
2018-04-02 13:33:08 +0200

    Init Day 9

---
+++ src/main/java/com/finix/alexcuva/kataday/day9/MaxDiffLength.java
@@ -0,0 +1,9 @@
+package com.finix.alexcuva.kataday.day9;
+
+class MaxDiffLength {
+
+    public static int mxdiflg(String[] a1, String[] a2) {
+        throw new UnsupportedOperationException();
+
+    }
+}
