bac8ef559ed830c64f609622390f2f685fa2d9db
Alexandre Cuva <alexandre.cuva@gmail.com>
2018-04-03 08:18:09 +0200

    Done day 9

--- src/main/java/com/finix/alexcuva/kataday/day9/MaxDiffLength.java
+++ src/main/java/com/finix/alexcuva/kataday/day9/MaxDiffLength.java
@@ -1,9 +1,24 @@
 package com.finix.alexcuva.kataday.day9;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 class MaxDiffLength {
 
     public static int mxdiflg(String[] a1, String[] a2) {
-        throw new UnsupportedOperationException();
+        if(a1 == null || a1.length == 0) {
+            return -1;
+        }
+        if(a2 == null || a2.length == 0) {
+            return -1;
+        }
+        String maxA1 = Arrays.stream(a1).max(Comparator.comparing(String::length)).get();
+        String minA1 = Arrays.stream(a1).min(Comparator.comparing(String::length)).get();
+        String minA2 = Arrays.stream(a2).min(Comparator.comparing(String::length)).get();
+        String maxA2 = Arrays.stream(a2).max(Comparator.comparing(String::length)).get();
+
+        // max(abs(length(x) − length(y)))
+        return Math.max(Math.abs(maxA1.length() - minA2.length()), Math.abs(minA1.length() - maxA2.length()));
 
     }
 }
--- src/test/java/com/finix/alexcuva/kataday/day9/MaxDiffLengthTest.java
+++ src/test/java/com/finix/alexcuva/kataday/day9/MaxDiffLengthTest.java
@@ -1,5 +1,6 @@
 package com.finix.alexcuva.kataday.day9;
 
+import org.assertj.core.api.Assertions;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
@@ -8,6 +9,20 @@ import org.junit.Test;
 
 @RunWith(JUnit4.class)
 public class MaxDiffLengthTest {
+
+    @Test
+    public void shouldReturnMinus1IfS1IsEmpty() {
+        String[] s1 = new String[0];
+        String[] s2 = new String[]{"cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"};
+        Assertions.assertThat(MaxDiffLength.mxdiflg(s1, s2)).isEqualTo(-1);
+    }
+
+    @Test
+    public void shouldReturnMinus1IfS2IsEmpty() {
+        String[] s2 = new String[0];
+        String[] s1 = new String[]{"cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"};
+        Assertions.assertThat(MaxDiffLength.mxdiflg(s1, s2)).isEqualTo(-1);
+    }
 
     @Test
     public void test() {
